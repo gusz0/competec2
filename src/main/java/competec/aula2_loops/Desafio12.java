2c6b5f3d53f98eb95dd37a3f72ef7829d11de6ad
gusz0 <166921337+gusz0@users.noreply.github.com>
2024-05-15 21:08:40 +0000

    Desafio senha resolvido

--- src/main/java/competec/aula2_loops/Desafio12.java
+++ src/main/java/competec/aula2_loops/Desafio12.java
@@ -6,10 +6,22 @@ public class Desafio12 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		String senha = "senha123"; // esta é a senha correta
-		String tentativa = "";
-
+		String senha = "12345"; // esta é a senha correta
+        String tentativa = "";
+        boolean senhaCorreta = false;
 		// faça o loop que lê as tentativas de senhas e verifica se elas estão corretas.
+		while (!senhaCorreta) {
+            System.out.println("Digite a senha:");
+            tentativa = scanner.nextLine();
+		
+
+		if (tentativa.equals(senha)) {
+			senhaCorreta = true;
+			System.out.println("Acesso permitido");
+		} else {
+			System.out.println("Acesso negado");
+		}
+		}
 		
 	}
 }

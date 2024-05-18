c3fcd9adb4772584ad9311208bb9b016468887af
gusz0 <166921337+gusz0@users.noreply.github.com>
2024-05-18 13:41:14 +0000

    desafio 21 e 22 feito

--- src/main/java/competec/aula2_loops/Desafio21.java
+++ src/main/java/competec/aula2_loops/Desafio21.java
@@ -6,5 +6,13 @@ public class Desafio21 {
 	public static void main (String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
+		System.out.println("Digite um número inteiro:");
+		int num = scanner.nextInt();
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(num * i);
+		}
+
+
 	}
 }
--- src/main/java/competec/aula2_loops/Desafio22.java
+++ src/main/java/competec/aula2_loops/Desafio22.java
@@ -6,5 +6,14 @@ public class Desafio22 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
+		System.out.println("Digite um número inteiro:");
+		int num = scanner.nextInt();
+
+		for (int impar = 0; impar <= num; impar++) {
+			if (impar%2 != 0) {
+				System.out.println(impar);
+			}
+
+
 	}
 }

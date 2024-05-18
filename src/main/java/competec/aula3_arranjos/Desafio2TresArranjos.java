25fbe0c1c3cde5074c51a9c91caacf4e4dc18921
gusz0 <166921337+gusz0@users.noreply.github.com>
2024-05-18 14:40:21 +0000

    SAIDA ARRANJOS

--- src/main/java/competec/aula3_arranjos/Desafio2TresArranjos.java
+++ src/main/java/competec/aula3_arranjos/Desafio2TresArranjos.java
@@ -3,11 +3,15 @@ package competec.aula3_arranjos;
 public class Desafio2TresArranjos {
     public static void main(String[] args) {
         // Crie um vetor com 3 números inteiros a sua escolha:
-
+        int numeros [] = new int [3];
+        numeros [0] = 24;
+        numeros [1] = 33;
+        numeros [2] = 69;
+        
         // Crie uma variável que irá armazenar a soma desses 3 valores:
-
+        int soma = numeros[0] + numeros[1] + numeros[2];
         // Imprima o resultado dessa variável:
-
+        System.out.println(soma);
         // Extra
         // [] - Faça uma estrutura de repeticao for ou while que imprima cada um dos valores
     }
--- src/main/java/competec/aula3_arranjos/Desafio3EntradaSaidaArranjos.java
+++ src/main/java/competec/aula3_arranjos/Desafio3EntradaSaidaArranjos.java
@@ -1,13 +1,21 @@
-package competec.aula3_arranjos;
+import java.util.Scanner;
+
+// package competec.aula3_arranjos;
 
 public class Desafio3EntradaSaidaArranjos {
 
 	public static void main(String[] args) {
-
+		Scanner scanner = new Scanner(System.in);
 		// Coletar tamanho arranjo
+		System.out.println("Digite o tamanho do arranjo:");
+		int tamanho = scanner.nextInt();
 
+		int arranjo [] = new int [tamanho];
 		// Preencher arranjo
-
+		System.out.println("Preencher arranjo:");
+		for (int iterador = 0; iterador < tamanho; iterador++) {
+			arranjo[iterador] = scanner.nextInt();
+		}
 		// Impremir arranjo
 
 		/*
